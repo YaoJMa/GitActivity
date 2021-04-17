8b32527df81b47eaf79e69ae7434b3c62fd7f2bd
Yao Ma <mayao@flip3.engr.oregonstate.edu>
2021-04-16 22:33:52 -0700

    Fixed syntax error

--- calc.py
+++ calc.py
@@ -21,19 +21,19 @@ number1 = int(input("Enter first number: "))
 number2 = int(input("Enter second number: "))
 
 if select == 1:
-    print(number_1, "+", number_2, "=",
-                    add(number_1, number_2))
+    print(number1, "+", number2, "=",
+                    add(number1, number2))
   
 elif select == 2:
-    print(number_1, "-", number_2, "=",
-                    sub(number_1, number_2))
+    print(number1, "-", number2, "=",
+                    sub(number1, number2))
   
 elif select == 3:
-    print(number_1, "/", number_2, "=",
-                    div(number_1, number_2))
+    print(number1, "/", number2, "=",
+                    div(number1, number2))
   
 elif select == 4:
-    print(number_1, "*", number_2, "=",
-                    mult(number_1, number_2))
+    print(number1, "*", number2, "=",
+                    mult(number1, number2))
 else:
     print("Invalid input")
