0e3108ff3997952367f4ae0f44268290ba778619
Yao Ma <mayao@flip3.engr.oregonstate.edu>
2021-04-16 22:25:17 -0700

    added instuctions for user

--- calc.py
+++ calc.py
@@ -8,6 +8,10 @@ def div(num1, num2):
 	return num1/num2
 
 def mult(num1, num2):	
-	return = num1 * num2
+	return num1 * num2
 
-
+print("Select an operation")
+print("1. Add")
+print("2. Subtract")
+print("3. Divide")
+print("4. Multiply")
