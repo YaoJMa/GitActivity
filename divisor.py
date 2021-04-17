055810807ba9c10df9bae7d702f4d2413ac19543
Yao Ma <mayao@flip3.engr.oregonstate.edu>
2021-04-16 22:59:58 -0700

    added divisor number and output

--- divisor.py
+++ divisor.py
@@ -1,8 +1,9 @@
 def divisor(n):
 	i = 1
-		while i <= n:
-			if (n % i==0):
-				print i, 
-			i = i + 1
+	while i <= n:	
+		if (n % i==0):
+			print(i) 
+		i = i + 1
 
-
+print("The divisors for 100 is: ")
+divisor(100)
